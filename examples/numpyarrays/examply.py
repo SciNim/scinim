68016563d939d78642e79a83e09ccc41982c831d
Clonkk <rf.clonk@linuxmail.org>
2024-06-12 20:04:05 +0200

    simplify python example to avoid slow it down; only keep the simplest & fastest loop. Perf goes from 90 seconds to 500/600 ms

--- examples/numpyarrays/examply.py
+++ examples/numpyarrays/examply.py
@@ -2,37 +2,18 @@ import examply
 import numpy as np
 from timeit import default_timer as timer
 
-def timeMe(text="func", func=None, arg=None):
-    start = timer()
-    res = func(arg)
-    end = timer()
-    print(text, " took : ", end-start, " seconds")
-    return res 
+def fLoop(ar):
+    s = 0.0
+    for i in range(0, len(ar)):
+        el = ar[i]
+        s = s + (1-el)/(1+el)
+    print("res=", s)
+    return s
 
 def main():
     print("Python => main()")
     MAX_LEN = int(5*1e8)
     ar = np.random.rand(MAX_LEN)
-
-    def fLoop(ar):
-        s = 0.0
-        for i in range(0, len(ar)):
-            el = ar[i] 
-            s = s + (1-el)/(1+el)
-        print("res=", s)
-        return s
-
-    def nditerLoop(ar):
-        s = 0.0
-        for el in np.nditer(ar):
-            s = s + (1-el)/(1+el)
-        print("res=", s)
-        return s
-
-    def listComp(ar):
-        x = np.cumsum([(1-el)/(1+el) for el in ar])
-        print("res=", x)
-        return s
 
 
     print("BEGIN")
@@ -42,25 +23,29 @@ def main():
     print("1)")
     # Value is consumed when passed to native code so in order re-use 'ar' we have to pass it by copy
     arCalc = ar.copy()
+    start = timer()
     res = examply.runCalc(arCalc)
-    print("Nim code took : res.elapsed=", res[0], " ms")
-    print("res.value=", res[1])
-    pyres = timeMe("ndIter loop", nditerLoop, ar)
-    print("pyres=", pyres)
+    end = timer()
+    print("Python measured native loop took : ", end-start, " seconds")
+    print("Nim measured native loop took : ", res[0], " ms")
+    print("res=", res[1])
 
-    # timeMe("python loop", fLoop, ar)
-    # timeMe("List comprehension", listComp, ar)
+    start = timer()
+    pyres = fLoop(ar)
+    end = timer()
+    print("Python loop took : ", end-start, " seconds")
+    print("pyres=", pyres)
 
     print("2)")
     arr = examply.modArrayInPlace(ar)
     print(arr)
-    # Note that arr is the same memory segment as ar since Nim code did not allocate 
+    # Note that arr is the same memory segment as ar since Nim code did not allocate
     print(np.shares_memory(ar, arr))
 
     print("----------------")
     print("END")
 
 main()
-## In bash, simply run : 
+## In bash, simply run :
 ## time nim c examply && time python3 examply.py > results.txt
 ## This takes about ~30 minutes due to long python loop
